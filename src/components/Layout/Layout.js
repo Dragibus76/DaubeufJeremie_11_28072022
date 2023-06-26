b7713e81c66dff986f7ace0e8af3507f756c1b71
CodeDragi <codedragi@gmail.com>
2023-06-26 13:22:28 +0200

    header & footer okay

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import Footer from "../Footer/Footer";
+import './Layout.css';
 
 
 export default function Layout() {
     return (
         <div className="container">
             <Header/>
+            <Footer/>
         </div>
     )
 }
