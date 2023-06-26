60b2c5448c5b1abe1c3c196bc8ee4c301c9c4887
CodeDragi <codedragi@gmail.com>
2023-06-26 14:06:51 +0200

    about page okay

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from './Dropdown.module.css';
+import { ReactComponent as Arrow } from '../../assets/images/dropdown/arrow.svg';
+
+export default function Dropdown({ summary, content }) {
+    const [ rotate, setRotate ] = useState(true);
+    const styleArrow = {
+        transform: rotate ? 'rotate(0)' : 'rotate(180deg)'
+    }
+
+    return (
+        <details className={styles.details}>
+            <summary onClick={() => setRotate(!rotate)}>{summary} <Arrow style={styleArrow}/></summary>
+            {
+                !Array.isArray(content)
+                ? (<p className={styles.content}>{content}</p>)
+                : (
+                    <ul className={styles.content}>
+                        {
+                            content.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))
+                        }
+                        </ul>
+                )
+            }
+        </details>
+    )
+}
+
---
+++ src/pages/About/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import styles from './About.module.css';
+import datadropdown from '../../utils/data/datadropdown.json';
+
+export default function About() {
+    return (
+        <main>
+            <Banner page={'about'} />
+            <div className={styles.containerDropdown}>
+                {
+                    datadropdown.map(({ summary, content }, index) => (
+                        <Dropdown key={index} summary={summary} content={content}/>
+                    ))
+                }
+            </div>
+        </main>
+    )
+}
