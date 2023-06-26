d6f3cb1af35e9023148238b6f3599a129f2bb7a9
CodeDragi <codedragi@gmail.com>
2023-06-26 13:44:36 +0200

    banners & home page okay

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Card.module.css';
+
+export default function Card({ id, title,cover}) {
+
+    const coverCard = {
+        background: `var(--linear), url(${cover}) no-repeat center`,
+        backgroundSize: 'cover'
+    }
+
+    return (
+        <Link className={styles.hostingCard} to={`/housing/${id}`} style={coverCard}>
+            <h2>{title}</h2>
+        </Link>
+    )
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import Banner from '../../components/Banner/Banner';
+import styles from './Home.module.css';
+import Card from '../../components/Card/Card';
+import housing from '../../utils/data/housing.json';
+
+export default function Home() {
+    return (
+        <main>
+            <Banner page={`home`}/>
+            <div className={styles.containerHousings}>
+                {
+                    housing.map(({id, title,cover}) => (
+                        <Card
+                        key={id}
+                        id={id}
+                        title={title}
+                        cover={cover}
+                        />
+                    ))
+                }
+            </div>
+        </main>
+    )
+}
